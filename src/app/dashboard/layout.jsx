3db790c2d7c5c458af202d87c6da4140287e8104
the-abed <mohammadabed1283@gmail.com>
2025-12-28 23:10:13 +0600

    add dashboard layout

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -5,7 +5,10 @@ const DashboardLayout = ({ children }) => {
   return (
     <div className="grid grid-cols-12 gap-4 min-h-screen">
       <div className="col-span-3 p-4 border-r-2">
-        <h4>Navigation</h4>
+                <Link href={"/"} className="text-lg font-semibold">
+          {" "}
+          🐳 Dev-Story
+        </Link>
         <div className="flex flex-col gap-4 mt-4">
           <Link
             className="bg-gray-500 px-4 py-2 rounded-3xl hover:scale-110 text-center"
@@ -27,7 +30,8 @@ const DashboardLayout = ({ children }) => {
           </Link>
         </div>
       </div>
-      <div className="col-span-10 p-4">{children}</div>
+      
+      <div className="col-span-9 p-4">{children}</div>
     </div>
   );
 };
