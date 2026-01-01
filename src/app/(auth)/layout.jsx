3075aba54ccda042af70ea62b8fa81689c515749
the-abed <mohammadabed1283@gmail.com>
2026-01-01 22:46:46 +0600

    add tutorial page

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import Title from "@/components/Title";
+import React from "react";
 
-const AuthLayout = ({children}) => {
-    return (
-        <div className='flex min-h-screen gap-5'>
-    
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen gap-5">
+      <div className="flex-2 ">{children}</div>
 
-            <div className='flex-1'>{children}</div>
-
-             <div lassName='flex-2'>
-                <h2>This is Auth Layout</h2>
-             </div>
-        </div>
-    );
+      <div className="flex-1 bg-sky-500 flex justify-center items-center p-5">
+        <h2 className="text-3xl font-bold">Welcome to DevStory</h2>
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
---
+++ src/app/tutorials/[...slug]/page.jsx
@@ -0,0 +1,22 @@
+import Title from "@/components/Title";
+import React from "react";
+
+const TutorialPage = async ({ params }) => {
+  //   const { technology, topic, page_no, subject } = await params;
+  const { slug } = await params;
+  const [technology, topic, , page_no, subject] = slug || [];
+
+  console.log(slug);
+  return (
+    <div>
+      <Title>{technology} tutorials</Title>
+      <h2>{topic}</h2> <hr />
+      <div className="flex justify-between">
+        <h3 className="font-bold">{subject}</h3>
+        <p>Page no: {page_no}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TutorialPage;
