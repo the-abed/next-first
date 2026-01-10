a37b8396fce03635749cd1d07737e3b5368a390f
the-abed <mohammadabed1283@gmail.com>
2026-01-10 22:38:43 +0600

    add navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import NavLink from "./NavLink";
 
 const Header = () => {
   const pathname = usePathname();
@@ -11,10 +12,10 @@ const Header = () => {
     <header className="px-4 py-3 border-b-2 border-gray-500 flex justify-between items-center flex-wr">
     <Link href="/" className="font-bold text-lg">🤖 Dev-Story</Link>
       <nav className="flex gap-4">
-        <Link href="/about">About</Link>
-        <Link href="/tutorials">Tutorials</Link>
-        <Link href="/login">Login</Link>
-        <Link href="/register">Register</Link>
+        <NavLink href="/about">About</NavLink>
+        <NavLink href="/tutorials">Tutorials</NavLink>
+        <NavLink href="/login">Login</NavLink>
+        <NavLink href="/register">Register</NavLink>
       </nav>
     </header>
   );
--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,10 +1,18 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-const NavLink = (href,children) => {
-    return (
-        <Link href={href}>{children}</Link>
-    );
+const NavLink = ({ href, children }) => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      className={`${pathname.startsWith(href) && "bg-sky-600 px-3 rounded-3xl"}`}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export default NavLink;
